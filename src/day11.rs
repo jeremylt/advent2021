6ec16f83d572bd521939f0b3785e31b8e3536b5b
Jeremy L Thompson <jeremy@jeremylt.org>
2021-12-11 02:52:57 -0700

    day11 - small improvements

--- src/day11.rs
+++ src/day11.rs
@@ -1,4 +1,9 @@
 //! Day 11:
+//! This solution relies upon knowledge of the grid size for performance.
+//! Without this knowledge, the grids need to be stored in Vecs instead
+//! of arrays. I used a recursive update around each point that triggered
+//! a flash. Most of the speedup came from homogonizing loops and
+//! reducing branching logic.
 
 use crate::prelude::*;
 
@@ -18,10 +23,12 @@ const OFFSETS: [i16; 8] = [
 // -----------------------------------------------------------------------------
 // Update energy state
 // -----------------------------------------------------------------------------
-fn update(energy: &mut Vec<u8>, visits: &mut Vec<u16>, index: usize, current_visit: u16) -> u16 {
-    if visits[index] == current_visit || visits[index] == 0 {
-        return current_visit;
-    };
+fn update(
+    energy: &mut [u8; WIDTH * WIDTH],
+    visits: &mut [u16; WIDTH * WIDTH],
+    index: usize,
+    current_visit: u16,
+) -> u16 {
     visits[index] = current_visit;
 
     if energy[index] == 10 {
@@ -43,13 +50,14 @@ fn update(energy: &mut Vec<u8>, visits: &mut Vec<u16>, index: usize, current_vis
 // -----------------------------------------------------------------------------
 // Count flashes
 // -----------------------------------------------------------------------------
-fn count_flashes(energy: &mut Vec<u8>) -> i32 {
-    let mut flash_count = 0;
-    (1..WIDTH - 1).for_each(|j| {
-        (1..WIDTH - 1).for_each(|i| {
+fn count_flashes(energy: &mut [u8; WIDTH * WIDTH]) -> i32 {
+    let flash_count = (1..WIDTH - 1).fold(0, |acc, j| {
+        acc + (1..WIDTH - 1).fold(0, |acc, i| {
             if energy[i + j * WIDTH] > 9 {
                 energy[i + j * WIDTH] = 0;
-                flash_count += 1;
+                acc + 1
+            } else {
+                acc
             }
         })
     });
@@ -59,13 +67,16 @@ fn count_flashes(energy: &mut Vec<u8>) -> i32 {
 // -----------------------------------------------------------------------------
 // Part 1
 // -----------------------------------------------------------------------------
-fn part_1(energy: &mut Vec<u8>, visits: &mut Vec<u16>) -> crate::Result<i32> {
+fn part_1(
+    energy: &mut [u8; WIDTH * WIDTH],
+    visits: &mut [u16; WIDTH * WIDTH],
+) -> crate::Result<i32> {
     let mut current_visit = 1;
     let mut flash_count = 0;
     (0..FIRST_GENERATIONS).for_each(|_| {
         (1..WIDTH - 1).for_each(|j| {
             (1..WIDTH - 1).for_each(|i| {
-                energy[i+j*WIDTH] += 1;
+                energy[i + j * WIDTH] += 1;
                 current_visit = update(energy, visits, i + j * WIDTH, current_visit + 1);
             })
         });
@@ -78,8 +89,8 @@ fn part_1(energy: &mut Vec<u8>, visits: &mut Vec<u16>) -> crate::Result<i32> {
 // Part 2
 // -----------------------------------------------------------------------------
 fn part_2(
-    energy: &mut Vec<u8>,
-    visits: &mut Vec<u16>,
+    energy: &mut [u8; WIDTH * WIDTH],
+    visits: &mut [u16; WIDTH * WIDTH],
     mut current_visit: u16,
 ) -> crate::Result<i32> {
     let mut generation = FIRST_GENERATIONS;
@@ -88,12 +99,11 @@ fn part_2(
         generation += 1;
         (1..WIDTH - 1).for_each(|j| {
             (1..WIDTH - 1).for_each(|i| {
-                energy[i+j*WIDTH] += 1;
+                energy[i + j * WIDTH] += 1;
                 current_visit = update(energy, visits, i + j * WIDTH, current_visit + 1);
             })
         });
-        let flash_count = count_flashes(energy);
-        all_flashed = flash_count as usize == (WIDTH - 2) * (WIDTH - 2);
+        all_flashed = count_flashes(energy) as usize == (WIDTH - 2) * (WIDTH - 2);
     }
     Ok(generation as i32)
 }
@@ -107,8 +117,8 @@ pub(crate) fn run(buffer: String) -> crate::Result<RunData> {
     // -------------------------------------------------------------------------
     // Read to vector
     let start_setup = Instant::now();
-    let mut energy: Vec<u8> = vec![0; WIDTH * WIDTH];
-    let mut visits: Vec<u16> = vec![0; WIDTH * WIDTH];
+    let mut energy: [u8; WIDTH * WIDTH] = [11; WIDTH * WIDTH];
+    let mut visits: [u16; WIDTH * WIDTH] = [0; WIDTH * WIDTH];
     buffer.lines().enumerate().for_each(|(j, line)| {
         line.as_bytes().iter().enumerate().for_each(|(i, b)| {
             energy[(i + 1) + (j + 1) * WIDTH] = *b - b'0';
@@ -130,7 +140,7 @@ pub(crate) fn run(buffer: String) -> crate::Result<RunData> {
     // -------------------------------------------------------------------------
     // Look for syncronization
     let start_part_2 = Instant::now();
-    let increases_2 = part_2(&mut energy, &mut visits, count_1 as u16 * 10)?;
+    let generation_2 = part_2(&mut energy, &mut visits, count_1 as u16 * 10)?;
     let time_part_2 = start_part_2.elapsed();
 
     // -------------------------------------------------------------------------
@@ -138,7 +148,7 @@ pub(crate) fn run(buffer: String) -> crate::Result<RunData> {
     // -------------------------------------------------------------------------
     Ok(RunData::new(
         count_1 as i64,
-        increases_2 as i64,
+        generation_2 as i64,
         Timing::new(
             time_setup,
             time_part_1,
@@ -154,7 +164,7 @@ pub(crate) fn run(buffer: String) -> crate::Result<RunData> {
 pub(crate) fn report(run_data: &RunData) -> crate::Result<()> {
     output::print_day(11, "Dumbo Octopus")?;
     output::print_part(1, "🐙 Count", &format!("{}", run_data.part_1))?;
-    output::print_part(2, "🐙 Increase", &format!("{}", run_data.part_2))?;
+    output::print_part(2, "🐙 Generation", &format!("{}", run_data.part_2))?;
     output::print_timing(&run_data.times)?;
     Ok(())
 }
